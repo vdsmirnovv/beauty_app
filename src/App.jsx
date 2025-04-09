9b70b260af1324dca7d184d5c9e3d26b2555224c
Vadim <75739522+vdsmirnovv@users.noreply.github.com>
2025-04-10 02:17:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import WebApp from "@twa-dev/sdk";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://beauty-back.onrender.com"; 
 
 console.log("API_URL:", API_URL);
 
